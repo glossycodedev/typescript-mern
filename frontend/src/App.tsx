a200efb5cefe146b9b47f167ac3d594bc7357456
glossycodedev <134337694+glossycodedev@users.noreply.github.com>
2024-09-09 00:10:27 +0300

    bootstrap and navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,44 @@
-import './App.css'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { sampleProducts } from './data'
 
 function App() {
- 
   return (
-    <div>
-      <header>TS Ecommerce</header>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>TS Ecommerce</Navbar.Brand>
+            <Nav>
+              <a href="/cart" className="nav-link">
+                Cart
+              </a>
+              <a href="/signin" className="nav-link">
+                Sign In
+              </a>
+            </Nav>
+          </Container>
+        </Navbar>
+      </header>
       <main>
-        {sampleProducts.map((product) => (
-          <li key={product.slug}>
-            <img
-              className="product-image"
-              src={product.image}
-              alt="product.name"
-            ></img>
-            <h2>{product.name}</h2>
-            <p>$ {product.price}</p>
-          </li>
-        ))}
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  className="product-image"
+                  src={product.image}
+                  alt="product.name"
+                ></img>
+                <h2>{product.name}</h2>
+                <p>$ {product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
-      <footer>@2025 All right reserved</footer>
+      <footer>
+        <div className="text-center">@2025 All right reserved</div>
+      </footer>
     </div>
   )
 }
